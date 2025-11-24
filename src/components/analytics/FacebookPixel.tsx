81e31eea21786835da1fe6b69de2be0cdc3db78c
Lucas <kamakurashorts@gmail.com>
2025-11-24 23:21:14 +0000

    ta dando isso ainda

--- src/components/analytics/FacebookPixel.tsx
+++ src/components/analytics/FacebookPixel.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import Script from 'next/script';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
@@ -13,16 +13,14 @@ declare global {
 }
 
 const FacebookPixel = () => {
+  const [loaded, setLoaded] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
-    if (!PIXEL_ID) return;
-    // This is to ensure the fbq function is available for subsequent page views
-    // after the initial script load.
-    if (window.fbq) {
+    if (loaded) {
       window.fbq('track', 'PageView');
     }
-  }, [pathname]);
+  }, [pathname, loaded]);
 
   if (!PIXEL_ID) {
     return null;
@@ -31,21 +29,14 @@ const FacebookPixel = () => {
   return (
     <>
       <Script
-        id="fb-pixel-sdk"
+        id="fb-pixel-base"
+        src="https://connect.facebook.net/en_US/fbevents.js"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
+        onLoad={() => {
+          window.fbq=window.fbq||function(){(window.fbq.q=window.fbq.q||[]).push(arguments)};
+          window.fbq.l=+new Date;
+          window.fbq('init', PIXEL_ID);
+          setLoaded(true);
         }}
       />
     </>
